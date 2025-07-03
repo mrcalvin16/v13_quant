00188281f0d7d169d68d4d4fc33c15ca0ef162e8
mrcalvin16 <kcalvin@tulane.edu>
2025-07-02 19:21:38 -0500

    Update ml_ensemble.py

--- prediction/ml_ensemble.py
+++ prediction/ml_ensemble.py
@@ -1,19 +1,3 @@
-import joblib
-import xgboost as xgb
-
-class MLEnsemble:
+ class MLEnsemble:
     def __init__(self):
         self.model = None
-
-    def train(self, X_train, y_train):
-        self.model = xgb.XGBRegressor(n_estimators=100)
-        self.model.fit(X_train, y_train)
-
-    def predict(self, X):
-        return self.model.predict(X)
-
-    def save(self, path):
-        joblib.dump(self.model, path)
-
-    def load(self, path):
-        self.model = joblib.load(path)
