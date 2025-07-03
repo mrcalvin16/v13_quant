d65365a960f25d2e1677c861c3b5ad79bd5c4ab1
mrcalvin16 <kcalvin@tulane.edu>
2025-07-02 19:03:27 -0500

    Update rl_agent.py

--- prediction/rl_agent.py
+++ prediction/rl_agent.py
@@ -1,20 +1,15 @@
-from stable_baselines3 import PPO
-import gym
-
 class RLAgent:
-    def __init__(self, env_name="CartPole-v1"):
-        self.env = gym.make(env_name)
-        self.model = PPO("MlpPolicy", self.env, verbose=0)
+    def __init__(self, env_name=None):
+        self.model = None
 
-    def train(self, timesteps=10000):
-        self.model.learn(total_timesteps=timesteps)
+    def train(self, timesteps=1000):
+        pass
 
     def predict(self, obs):
-        action, _ = self.model.predict(obs)
-        return action
+        return 0.5  # Dummy prediction
 
     def save(self, path):
-        self.model.save(path)
+        pass
 
     def load(self, path):
-        self.model = PPO.load(path)
+        pass
