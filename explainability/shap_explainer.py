cca15bb83c9d8033799dfdc24c61214ecbecf78d
mrcalvin16 <kcalvin@tulane.edu>
2025-07-07 02:47:12 -0500

    Delete explainability directory

--- explainability/shap_explainer.py
+++
@@ -1,9 +0,0 @@
-import shap
-
-class SHAPExplainer:
-    def __init__(self, model):
-        self.explainer = shap.Explainer(model)
-
-    def explain(self, X):
-        shap_values = self.explainer(X)
-        return shap_values
