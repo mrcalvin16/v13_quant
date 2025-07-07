49efce310524c92db818597cb4264e47508d0874
mrcalvin16 <kcalvin@tulane.edu>
2025-07-07 02:47:02 -0500

    Delete execution directory

--- execution/crypto.py
+++
@@ -1,6 +0,0 @@
-class CryptoExecutor:
-    def __init__(self, config):
-        self.client = None
-
-    def submit_order(self, symbol, qty, side):
-        return {"message": "Dummy Binance order submitted (testing mode)."}
